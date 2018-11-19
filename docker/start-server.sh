bc00099f76f302954f8a1225ca429ef857722c8b
veysiertekin <ertekin.veysi@gmail.com>
2018-11-20 02:16:29 +0300

    fixes a typo

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Execute task just on job manager
+# Execute task just on the job manager
 if [ "$1" = "jobmanager" ]; then
     until flink run /job.jar --csvFile "$CSV_FILE" --outputPath "$OUTPUT_PATH"; do
       echo "Flink is not active. Will be waiting for 2 seconds to retry..."
